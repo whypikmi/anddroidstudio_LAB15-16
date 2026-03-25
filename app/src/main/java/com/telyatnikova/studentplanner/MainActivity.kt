3b9eee35b7a645c0a0a02fbfaf3855b052afca76
ElizavetaTelyatnikova <ttelatnikova1@yandex.ru>
2026-03-26 00:10:34 +0300

    Step 7: Setup NavHost and complete navigation

--- app/src/main/java/com/telyatnikova/studentplanner/MainActivity.kt
+++ app/src/main/java/com/telyatnikova/studentplanner/MainActivity.kt
@@ -7,10 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.telyatnikova.studentplanner.navigation.StudentPlannerNavHost
 import com.telyatnikova.studentplanner.ui.theme.StudentPlannerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +18,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             StudentPlannerTheme {
+                val navController = rememberNavController()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    StudentPlannerNavHost(
+                        navController = navController,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StudentPlannerTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/telyatnikova/studentplanner/navigation/NavGraph.kt
@@ -0,0 +1,68 @@
+package com.telyatnikova.studentplanner.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.telyatnikova.studentplanner.ui_model.*
+
+@Composable
+fun StudentPlannerNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route,
+        modifier = modifier
+    ) {
+        composable(route = Screen.Home.route) {
+            HomeScreen(
+                onSubjectClick = { subjectId ->
+                    navController.navigate(Screen.Details.createRoute(subjectId))
+                },
+                onProfileClick = {
+                    navController.navigate(Screen.Profile.route)
+                },
+                onSettingsClick = {
+
+                    navController.navigate(Screen.Settings.route)
+                }
+            )
+        }
+        composable(
+            route = Screen.Details.route,
+            arguments = listOf(
+                navArgument("subjectId") {
+                    type = NavType.StringType
+                }
+            )
+        ) { backStackEntry ->
+            val subjectId = backStackEntry.arguments?.getString("subjectId") ?: ""
+            DetailsScreen(
+                subjectId = subjectId,
+                onNavigateBack = {
+                    navController.popBackStack()
+                }
+            )
+        }
+        composable(route = Screen.Profile.route) {
+            ProfileScreen(
+                onNavigateBack = {
+                    navController.popBackStack()
+                })
+        }
+        composable(route = Screen.Settings.route) {
+            SettingsScreen(
+                onNavigateBack = {
+                    navController.popBackStack()
+                }
+            )
+        }
+    }
+}
