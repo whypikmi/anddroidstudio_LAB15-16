87e139814a0153f9e5b893546bd58ca3140bde3a
ElizavetaTelyatnikova <ttelatnikova1@yandex.ru>
2026-03-25 18:24:04 +0300

    Step 3: Create Subject data model and sample data

---
+++ app/src/main/java/com/telyatnikova/studentplanner/data/Subject.kt
@@ -0,0 +1,28 @@
+package com.telyatnikova.studentplanner.data
+
+data class Subject(
+    val id: String,
+    val name: String,
+    val professor: String,
+    val credits: Int,
+    val currentGrade: String,
+    val description: String
+)
+val sampleSubjects = listOf(
+    Subject(
+        id = "1",
+        name = "Разработка мобильных приложений",
+        professor = "Леонтьев Д.А.",
+        credits = 91,
+        currentGrade = "Отлично",
+        description = "Разработка нативных Android-приложений с использованием Kotlin и современных компонентов Jetpack Compose. Изучение архитектурных паттернов MVVM, работы с локальными базами данных и сетевыми API."
+    ),
+    Subject(
+        id = "3",
+        name = "Системное программирование",
+        professor = "Токаев Т.И.",
+        credits = 75,
+        currentGrade = "Хорошо",
+        description = "Низкоуровневая разработка на языке Rust: управление памятью без сборщика мусора, многопоточность, работа с операционной системой и создание высокопроизводительных системных компонентов."
+    )
+)
